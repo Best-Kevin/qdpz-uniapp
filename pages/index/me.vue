13a2daa1e70e3822ddc3cee0448529aa1a36ec11
凯文童鞋丶 <280224091@qq.com>
2021-05-16 00:42:58 +0800

    update pages/index/me.vue.

--- pages/index/me.vue
+++ pages/index/me.vue
@@ -205,22 +205,22 @@
 				modalName: null,
 				picName: '流星之夜',
 				pic: [{
-					link: '../../static/me/zjx_me_bg1.jpeg',
+					link: 'https://cdn.zhoukaiwen.com/zjx_me_bg1.jpeg',
 					name: '春天'
 				}, {
-					link: '../../static/me/zjx_me_bg2.jpeg',
+					link: 'https://cdn.zhoukaiwen.com/zjx_me_bg2.jpeg',
 					name: '夏天'
 				}, {
-					link: '../../static/me/zjx_me_bg3.jpeg',
+					link: 'https://cdn.zhoukaiwen.com/zjx_me_bg3.jpeg',
 					name: '秋天'
 				}, {
-					link: '../../static/me/zjx_me_bg4.jpeg',
+					link: 'https://cdn.zhoukaiwen.com/zjx_me_bg4.jpeg',
 					name: '冬天'
 				}, {
-					link: '../../static/me/zjx_me_bg5.jpeg',
+					link: 'https://cdn.zhoukaiwen.com/zjx_me_bg5.jpeg',
 					name: '幽静'
 				}, {
-					link: '../../static/me/zjx_me_bg6.jpg',
+					link: 'https://cdn.zhoukaiwen.com/zjx_me_bg6.jpg',
 					name: '天空'
 				}],
 				topBackGroupImageIndex: 5,
