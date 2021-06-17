c1e62518d7c1230ee9dff2b53ee909a3dfbe274e
凯文童鞋丶 <280224091@qq.com>
2021-06-17 11:35:03 +0800

    June17th：增加引用文件的注释说明

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 
+// 此处为引用自定义顶部
 import cuCustom from './colorui/components/cu-custom.vue'
 Vue.component('cu-custom',cuCustom);
 import TnCustom from './components/TnCustom/TnCustom.vue'
